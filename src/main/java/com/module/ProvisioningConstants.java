5804f7ab842bcee0d2c59e2d80c617679ce75fab
Karen Kirakosyan <karen_ki@instigatemobil.com>
2019-12-17 14:32:22 +0400

    Added constants

---
+++ src/main/java/com/module/ProvisioningConstants.java
@@ -0,0 +1,7 @@
+package com.module;
+
+public class ProvisioningConstants {
+    public static final String PROVISION_REQUESTING = "provision_requesting";
+    public static final String PROVISIONING_DATA = "provisioning_data";
+    public static final String KEY_PROVISIONING_DATA = "key_provisioning_data";
+}
